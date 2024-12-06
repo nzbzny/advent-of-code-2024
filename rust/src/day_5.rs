f725353ad7a1364f155340466a0b82e2b794db02
nzbozny <nezbozny@hotmail.com>
2024-12-06 14:49:32 -0500

    day 5 part 1

--- rust/src/day_5.rs
+++ rust/src/day_5.rs
@@ -2,20 +2,29 @@ use std::collections::{HashMap, HashSet};
 
 use crate::utils;
 
-fn parse_ruleset(rules_str: &[String]) -> Option<HashMap<String, HashSet<String>>> {
+fn parse_ruleset(rules_str: &[String]) -> HashMap<String, HashSet<String>> {
     let mut ruleset: HashMap<String, HashSet<String>> = HashMap::new();
 
     for rule in rules_str {
-        let Some((first, other)) = rule.split_once('|') else { return None };
+        let (first, other) = rule.split_once('|').unwrap();
+
+        match ruleset.get_mut(first) {
+            Some(set) => {
+                set.insert(other.to_owned());
+            }
+            None => {
+                ruleset.insert(first.to_owned(), HashSet::from([other.to_owned()]));
+            }
+        };
     }
 
-    Some(ruleset)
+    ruleset
 }
 
 pub fn run() {
     let lines = utils::parse_file("day_5");
     
-    let split_idx = match lines.iter().enumerate().find(|(_, s)| { s.is_empty() }) {
+    let split_idx = match lines.iter().enumerate().find(|(_, s)| { !s.contains('|') }) {
         Some((idx, _)) => idx,
         None => {
             println!("Could not find split_idx");
@@ -24,4 +33,30 @@ pub fn run() {
     };
 
     let (rules_str, printed) = lines.split_at(split_idx);
+    let ruleset = parse_ruleset(rules_str);
+    let mut sum = 0;
+
+    for manual in printed {
+        let mut pages = manual.split(',');
+        let mut seen: HashSet<String> = HashSet::new();
+        let mut valid = true;
+        let mut count = 0;
+
+        pages.clone().for_each(|page| {
+            if let Some(rules) = ruleset.get(page) {
+                if !rules.is_disjoint(&seen) {
+                    valid = false;
+                }
+            }
+
+            seen.insert(page.to_owned());
+            count += 1;
+        });
+
+        if valid {
+            sum += pages.nth(count / 2).unwrap().parse::<i64>().unwrap();
+        }
+    }
+
+    println!("Sum is: {sum}");
 }
