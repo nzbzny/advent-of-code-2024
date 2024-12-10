072c5e8a0d5a0d266e5a97653bdb2ef738b72eb3
nzbozny <nezbozny@hotmail.com>
2024-12-09 22:13:05 -0500

    part 1

--- rust/src/day_7.rs
+++ rust/src/day_7.rs
@@ -1,38 +1,47 @@
 use crate::utils;
 
+#[derive(Debug)]
 struct Equation {
-    result: i64,
-    values: Vec<i64>
+    result: u64,
+    values: Vec<u64>
 }
 
 fn create_equations(lines: Vec<String>) -> Vec<Equation> {
     lines.iter().map(|line| {
         let (res, vals) = line.split_once(':').expect("Could not split line {line}");
-        let result = res.parse::<i64>().unwrap();
-        let values = vals.split(' ').map(|val| val.parse::<i64>().unwrap()).collect();
+        let result = res.parse::<u64>().unwrap();
+        let values = vals.trim().split(' ').map(|val| val.parse::<u64>().unwrap()).collect();
 
         Equation{ result, values }
     }).collect()
 }
 
-fn is_equation_valid(eq: &Equation) -> bool {
-    let mut sum = 0;
-
-    for val in &eq.values {
-
+fn is_equation_valid(eq: &Equation, param_idx: usize, curr_val: u64) -> bool {
+    if param_idx == eq.values.len() {
+        return curr_val == eq.result;
     }
 
-    true
+    let add = curr_val + eq.values[param_idx];
+    if is_equation_valid(eq, param_idx + 1, add) {
+        return true;
+    }
+
+    let mult = if param_idx == 0 { eq.values[param_idx] } else { curr_val * eq.values[param_idx] };
+    if is_equation_valid(eq, param_idx + 1, mult) {
+        return true;
+    }
+    
+    false
 }
 
 pub fn run() {
-    // let lines = utils::parse_file("day_7");
-    let lines = utils::parse_file("test");
+    let lines = utils::parse_file("day_7");
+    // let lines = utils::parse_file("test");
 
     let equations = create_equations(lines);
 
     let sum = equations.iter().fold(0, |acc, eq| {
-        if is_equation_valid(eq) {
+        if is_equation_valid(eq, 0, 0) {
             acc + eq.result
         } else {
             acc
