fd87820ad461a0914ffab7c4eab56c5f55851aef
nzbozny <nezbozny@hotmail.com>
2024-12-15 00:24:01 -0500

    keep working on part 2

--- rust/src/day_12.rs
+++ rust/src/day_12.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashSet;
 
 use crate::utils;
 
@@ -45,8 +45,8 @@ fn identify_regions(map: &Vec<Vec<char>>) -> Vec<Vec<Position>>{
 
     for y in 0..map.len() {
         for x in 0..map[0].len() {
-            let region = perform_dfs(&map, &Position{ x, y }, map[y][x], &mut seen);
-            if region.len() > 0 {
+            let region = perform_dfs(map, &Position{ x, y }, map[y][x], &mut seen);
+            if !region.is_empty() {
                 result.push(region);
             }
         }
@@ -55,7 +55,7 @@ fn identify_regions(map: &Vec<Vec<char>>) -> Vec<Vec<Position>>{
     result
 }
 
-fn is_same_plant(map: &Vec<Vec<char>>, pos: &Position, plant: char) -> bool {
+fn is_same_plant(map: &[Vec<char>], pos: &Position, plant: char) -> bool {
     if pos.y >= map.len() || pos.x >= map[0].len() {
         return false;
     }
@@ -63,7 +63,7 @@ fn is_same_plant(map: &Vec<Vec<char>>, pos: &Position, plant: char) -> bool {
     map[pos.y][pos.x] == plant
 }
 
-fn calculate_region_perimeter(map: &Vec<Vec<char>>, region: &[Position]) -> usize {
+fn calculate_region_perimeter(map: &[Vec<char>], region: &[Position]) -> usize {
     let plant = map[region[0].y][region[0].x];
     
     region.iter().fold(0, |mut acc, plot| {
@@ -92,14 +92,14 @@ fn calculate_region_perimeter(map: &Vec<Vec<char>>, region: &[Position]) -> usiz
     })
 }
 
-#[derive(Clone, Eq, Hash, PartialEq)]
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
 struct LineSegment {
     p1: Position,
     p2: Position,
 }
 
 fn get_region_outside_line_segments(region: &[Position]) -> HashSet<LineSegment> {
-    let region_set: HashSet<Position> = HashSet::from_iter(region.iter().cloned());
+    let region_set: HashSet<Position> = region.iter().cloned().collect();
     let mut result = HashSet::new();
 
     for plot in region {
@@ -118,6 +118,8 @@ fn get_region_outside_line_segments(region: &[Position]) -> HashSet<LineSegment>
             if !region_set.contains(&Position{ x: plot.x, y }) {
                 result.insert(LineSegment { p1: Position { x: plot.x, y: plot.y }, p2: Position { x: plot.x + 1, y: plot.y } });
             }
+        } else {
+            result.insert(LineSegment { p1: Position { x: plot.x, y: plot.y }, p2: Position { x: plot.x + 1, y: plot.y } });
         }
 
         // left
@@ -125,21 +127,62 @@ fn get_region_outside_line_segments(region: &[Position]) -> HashSet<LineSegment>
             if !region_set.contains(&Position{ x, y: plot.y }) {
                 result.insert(LineSegment { p1: Position { x: plot.x, y: plot.y }, p2: Position { x: plot.x, y: plot.y + 1 } });
             }
+        } else {
+            result.insert(LineSegment { p1: Position { x: plot.x, y: plot.y }, p2: Position { x: plot.x, y: plot.y + 1 } });
         }
     }
 
     result
 }
 
+fn contains_vertical_from_point(pos: &Position, edges: &HashSet<LineSegment>) -> bool {
+    if edges.contains(&LineSegment{ p1: pos.clone(), p2: Position{ x: pos.x, y: pos.y + 1 }}) {
+        true
+    } else if let Some(y) = pos.y.checked_sub(1) {
+        edges.contains(&LineSegment{ p1: Position{ x: pos.x, y }, p2: pos.clone() })
+    } else {
+        false
+    }
+}
+
+fn contains_horizontal_from_point(pos: &Position, edges: &HashSet<LineSegment>) -> bool {
+    if edges.contains(&LineSegment{ p1: pos.clone(), p2: Position{ x: pos.x + 1, y: pos.y }}) {
+        true
+    } else if let Some(x) = pos.x.checked_sub(1) {
+        edges.contains(&LineSegment{ p1: Position{ x, y: pos.y }, p2: pos.clone() })
+    } else {
+        false
+    }
+}
+
 fn count_region_sides(region: &[Position]) -> usize {
     let edges = get_region_outside_line_segments(region);
-    let mut count = 0;
+    let mut vertices: HashSet<Position> = HashSet::new();
 
-    for edge in edges {
-        // check if edge is a corner
+    for edge in &edges {
+        // horizontal
+        if edge.p1.x == edge.p2.x {
+            if contains_horizontal_from_point(&edge.p1, &edges) {
+                vertices.insert(edge.p1.clone());
+            }
+
+            if contains_horizontal_from_point(&edge.p2, &edges) {
+                vertices.insert(edge.p2.clone());
+            }
+        } else {
+            if contains_vertical_from_point(&edge.p1, &edges) {
+                vertices.insert(edge.p1.clone());
+            }
+
+            if contains_vertical_from_point(&edge.p2, &edges) {
+                vertices.insert(edge.p2.clone());
+            }
+        }
     }
+    // println!("{:?}", edges);
+    // println!("{:?}", vertices);
 
-    count
+    vertices.len()
 }
 
 pub fn run() {
@@ -153,8 +196,19 @@ pub fn run() {
     let regions = identify_regions(&map);
 
     let price = regions.iter().fold(0, |acc, region| {
-        acc + (calculate_region_perimeter(&map, &region) * region.len())
+        acc + (calculate_region_perimeter(&map, region) * region.len())
     });
 
     println!("Price is: {price}");
+
+    let discount_price = regions.iter().fold(0, |acc, region| {
+        let x = count_region_sides(region);
+        let y = region.len();
+
+        // println!("region of {} plants with price {y} * {x} = {}", map[region[0].y][region[0].x], x * y);
+
+        acc + (x * y)
+    });
+
+    println!("Discounted price is: {discount_price}");
 }
